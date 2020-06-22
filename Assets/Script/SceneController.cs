7b796805e1d335a39f2871aba2d911a86fcc26d8
yumi <cblog1201@gmail.com>
2020-06-22 17:43:37 +0900

    ゲームのプロジェクトです

---
+++ Assets/Script/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	public void PushStartButton()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+    public void PushOptionButton()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
+    public void PushExitButton()
+    {
+        Application.Quit();
+       
+    }
+    public void PushStage1()
+    {
+        SceneManager.LoadScene("SearchScene1");
+    }
+
+    public void PushBackButton()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+   
+}
